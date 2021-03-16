0989b57240d99c52a1927ebd4f1af8e14f2471a4
Надежда Чернобай <45025319+Kiiwiii@users.noreply.github.com>
2021-03-16 15:06:17 +0500

    Add files via upload
    Выполнение домашнего задания №1. Урок - 1.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();
+            DateTime date = DateTime.Now;
+            Console.WriteLine($"Привет, {name}, сейчас {date}");
+
+            Console.ReadKey();
+        }
+    }
+}
